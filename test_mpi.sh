142f43386de3e9d016ef36c7d508ddb4186fd07b
Ahsan Zaman <ahsanzam@usc.edu>
2018-04-30 20:07:17 -0700

    test script

--- test_mpi.sh
+++ test_mpi.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
 
 tests=(spheres table siggraph)
 
 for test in ${tests[@]};
 	do
-	echo Results for MPI Parallel $test.txt on $1 > Results/$test_MPI_$1.txt;
+	echo "Results for MPI Parallel "$test".txt on" $1 > Results/$test_MPI_$1.txt;
 	for number in {1..80};
 		do
 			mpirun mpi tests/$test.txt $test_MPI_$1.bmp $number >> Results/$test_MPI_$1.txt;
 		done;
-	echo Completed running test $test. Output is in Results/$test_MPI_$1.txt.
+	echo "Completed running test "$test". Output is in Results/"$test_MPI_$1".txt."
 	done
